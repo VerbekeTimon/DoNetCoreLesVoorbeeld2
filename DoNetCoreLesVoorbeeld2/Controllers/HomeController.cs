d23ff73c0a05f2f5535c5d5b63fd7bbcddf6df07
VerbekeTimon <timon.verbeke@student.howest.be>
2018-02-19 11:55:59 +0100

    created view and viewbag

--- DoNetCoreLesVoorbeeld2/Controllers/HomeController.cs
+++ DoNetCoreLesVoorbeeld2/Controllers/HomeController.cs
@@ -8,9 +8,28 @@ namespace DoNetCoreLesVoorbeeld2.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index(string name)
+        private string[] groenten = {
+            "Rode kool",
+            "Spruitjes",
+            "Wortel",
+            "Spinazie" 
+        };
+
+        public ViewResult Index(string name)
         {
-            return $"Hello {name}";
+            ViewBag.Groet = DateTime.Now.Hour < 12 ? "Goeiemorgen" : "Goeienamiddag";
+            return View();
+        }
+
+        public ViewResult Groenten(string zoekGroente)
+        {
+            ViewBag.Groenten = groenten;
+            if (!string.IsNullOrEmpty(zoekGroente))
+            {
+                ViewBag.Zoekresultaat = $"De gezochte groente is de " +
+                                        $"{Array.IndexOf(groenten, zoekGroente) + 1}e uit de lijst.";
+            }
+            return View();
         }
     }
 }
